6ba66230acf47d3eba7b1bb336dd7db6aa92e80c
Timothy Cyrus <4201229+tcyrus@users.noreply.github.com>
2022-12-04 15:37:18 -0500

    Update requester.py

--- AO3/requester.py
+++ AO3/requester.py
@@ -1,5 +1,6 @@
 import threading
 import time
+from typing import Optional
 
 import requests
 
@@ -7,26 +8,52 @@ import requests
 class Requester:
     """Requester object"""
     
-    def __init__(self, rqtw=-1, timew=60):
+    def __init__(
+        self,
+        rqtw: Optional[int] = -1,
+        timew: Optional[int] = 60
+    ):
         """Limits the request rate to prevent HTTP 429 (rate limiting) responses.
         12 request per minute seems to be the limit.
 
         Args:
-            rqm (int, optional): Maximum requests per time window (-1 -> no limit). Defaults to -1.
-            timew (int, optional): Time window (seconds). Defaults to 60.
+            rqtw: Maximum requests per time window (-1 -> no limit). Defaults to -1.
+            timew: Time window (seconds). Defaults to 60.
         """
         
+        if rqtw is None:
+            rqtw = -1
+
+        if timew is None:
+            timew = 60
+
         self._requests = []
         self._rqtw = rqtw
         self._timew = timew
         self._lock = threading.Lock()
         self.total = 0
-        
-    def setRQTW(self, value):
+
+    @property
+    def rqtw(self) -> int:
+        return self._rqtw
+
+    @rqtw.setter
+    def rqtw(self, value: int) -> None:
         self._rqtw = value
-        
-    def setTimeW(self, value):
+
+    def setRQTW(self, value: int) -> None:
+        self.rqtw = value
+
+    @property
+    def timew(self) -> int:
+        return self._timew
+
+    @rqtw.setter
+    def timew(self, value: int) -> None:
         self._timew = value
+
+    def setTimeW(self, value: int) -> None:
+        self.timew = value
 
     def request(self, *args, **kwargs):
         """Requests a web page once enough time has passed since the last request
@@ -37,7 +64,7 @@ class Requester:
         Returns:
             requests.Response: Response object
         """
-        
+
         # We've made a bunch of requests, time to rate limit?
         if self._rqtw != -1:
             with self._lock:
@@ -59,14 +86,14 @@ class Requester:
                 if self._rqtw != -1:
                     self._requests.append(time.time())
                 self.total += 1
-                           
+
         if "session" in kwargs:
             sess = kwargs["session"]
             del kwargs["session"]
             req = sess.request(*args, **kwargs)
         else:
             req = requests.request(*args, **kwargs)
-            
+
         return req
 
 requester = Requester()
